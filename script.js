8971fee280d5940e115913f817d29b038ef4a20a
Ubuntu <ubuntu@ip-172-31-40-131.us-west-2.compute.internal>
2013-10-09 17:58:22 +0000

    more responsive + social media links

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(window).load(function() {
-    $(".screenplay").delay(0).fadeOut(0, function(){ 
-      $(".fade-in").fadeIn(0); 
+    $(".screenplay").delay(5000).fadeOut(1000, function(){ 
+      $(".fade-in").fadeIn(4000); 
       }); 
   });
 
