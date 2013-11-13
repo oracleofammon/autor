549eda0a29d2d63e28ee19a530e7418d9f8a2faf
Ubuntu <ubuntu@ip-172-31-40-131.us-west-2.compute.internal>
2013-11-13 14:24:55 +0000

    fixed social buttons and made more responsive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(window).load(function() {
     $(".fade-in").hide(); 
-    $(".screenplay").delay(5000).fadeOut(1000, function(){ 
+    $(".screenplay").delay(0).fadeOut(1000, function(){ 
       $(".fade-in").fadeIn(0); 
       }); 
   });      
